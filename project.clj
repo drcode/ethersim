7d1b9a31d391d17006c06aa428c9cdba77e4d613
Conrad Barski <lisperati@gmail.com>
2014-10-29 19:39:58 +0000

    some more cleanup

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject ethersim "0.1.0-SNAPSHOT"
+(defproject ethersim "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main ethersim.core
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clll "0.1.0-SNAPSHOT"]])
+                 [clll "0.1.2alpha"]])
 
 
--- src/ethersim/core.clj
+++ src/ethersim/core.clj
@@ -1,10 +1,7 @@
 (ns ethersim.core
     (:require [clojure.string :refer [upper-case]]
               [clojure.repl :refer [doc]]
-              [clojure.pprint :refer [print-table]]
-              [clll-examples.core :refer [key-value-publisher]]))
-
-(def kvp key-value-publisher)
+              [clojure.pprint :refer [print-table]]))
 
 (defn out [& args]
       (print (apply str args))
@@ -38,7 +35,7 @@
                                                       (let [s (read-line)]
                                                            (if (= s "")
                                                                0
-                                                               (Integer. s))))
+                                                               (read-string s))))
             data                                  (do (out "Data (Hit enter when done)\n")
                                                       (vec (reverse (loop [acc []]
                                                                           (let [s (read-line)]
@@ -76,7 +73,7 @@
                                                       (read-line))]))
             contract-info    (into {}
                                    (for [contract contract-names]
-                                        [contract (let [{:keys [storage result]} ((eval contract) {} {:caller (contract-authors contract)})]
+                                        [contract (let [{:keys [storage result]} ((eval contract) {} {:caller (contract-authors contract)} {:hash 345 :number 1})]
                                                        {:storage storage 
                                                         :fun     (first result)})]))
             caller (do (out "starting caller: ")
@@ -99,3 +96,5 @@
                        (do (out "invalid command\n")
                            (recur contract-info blocknum caller))))))
 
+(defn -main []
+      nil)
